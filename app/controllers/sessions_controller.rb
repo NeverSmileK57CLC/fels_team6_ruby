2cdee3772b2d9e12bdc782b01b3f3e99fa07def9
NeverSmileK57CLC <neversmile12b1@gmail.com>
2015-11-19 19:08:20 +0700

    Config api signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,17 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.authenticate(params[:session][:email], params[:session][:password])
+    #render json: user
     respond_to do |format|
     	if user.nil?
     		@title = "Sign in"
     		format.html {redirect_to signin_path, alert: "Invalid email/password combination."}
-        format.json {render json: {message: "not found user"}, status: "fail"}
+        format.json {render json: {message: "not found user", status: "fail"}}
     	else
-        byebug
-    		sign_in user
+    		#sign_in user
     		format.html {redirect_to user_path(user), notice: "Sign in successfull."}
-        format.json {render json: {user: user.as_json}, status: "success"}
+        format.json {render json: {user: user, status: "success"}}
+        #format.json {render json: "Bach Ngoc"}
     	end
     end
   end
